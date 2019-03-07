abdb32fc26a1ab60e479a2b5e7ab1590ecd955cd
Joicy George <joicygeorge510@gmail.com>
2019-03-07 18:41:47 +0530

    User authorisation  feature added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 # post.rb
 class Post < ApplicationRecord
+  belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
