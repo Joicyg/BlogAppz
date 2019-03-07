58bac6b0369ec59bfe22c74145701a917190cd1e
Joicy George <joicygeorge510@gmail.com>
2019-03-07 10:30:04 +0530

    Error rectified with email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  # config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDMAIL_USERNAME'],
     password: ENV['SENDMAIL_PASSWORD'],
